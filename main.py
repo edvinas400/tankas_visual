2d6dc8cc5c2a7b6e0c53cf62ac7b53df59a86fa2
Edvinas Jarmalavicius <edvinas400@gmail.com>
2023-01-03 15:31:41 +0200

    done?

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import pygame
-from tankas import Tankas, pickle
+import pygame, pickle
+from tankas import Tankas, math
 
 pygame.init()
+try:
+    with open("rezultatai.pkl", "rb") as file:
+        highscore = pickle.load(file)
+except:
+    highscore = 0
 
 gamesizex = 800
 gamesizey = 600
@@ -14,7 +19,12 @@ pygame.display.set_icon(icon)
 
 a = pygame.image.load("tankas.png")
 kulka = pygame.image.load("kulka.png")
-suvis = "Laukia"
+enemy = pygame.image.load("enemy2.png")
+fire = pygame.image.load("fire.png")
+tekstas = pygame.font.Font("sketch.otf", 30)
+tekstas2 = pygame.font.Font("sketch.otf", 100)
+
+busena = "Laukia"
 
 tankas = Tankas(gamesizex, gamesizey, a, kulka)
 
@@ -24,9 +34,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                suvis = "Issauta"
-                tankas.suvis()
-                kulka = tankas.kulka
+                if busena == "Laukia":
+                    busena = "Issauta"
+                    boom = pygame.mixer.Sound("laser2.wav")
+                    boom.play()
+                    tankas.suvis()
+                    kulka = tankas.kulka
             if event.key in (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s):
                 lastkey = event.key
             match lastkey:
@@ -46,32 +59,50 @@ while running:
 
     tankas.stepx = -0.1 if lastkey == pygame.K_a else 0.1 if lastkey == pygame.K_d else 0
     tankas.stepy = -0.1 if lastkey == pygame.K_w else 0.1 if lastkey == pygame.K_s else 0
-    tankas.koordinates()
-    if suvis == "Issauta":
+
+    if busena == "Issauta":
         screen.blit(kulka, (tankas.kulkax, tankas.kulkay))
         tankas.kulkax += tankas.stepkulkax
-        tankas.kulkay +=tankas.stepkulkay
+        tankas.kulkay += tankas.stepkulkay
+        if tankas.kulkax < 0 or tankas.kulkay < 0 or tankas.kulkax > gamesizex or tankas.kulkay > gamesizey:
+            busena = "Laukia"
+
+    tankas.koordinates()
+    screen.blit(tankas.pozicija, (tankas.x, tankas.y))
+    screen.blit(tekstas.render("Your score: " + str(tankas.score), True, (145, 135, 225)), (2, 2))
+    screen.blit(tekstas.render("Highscore: " + str(highscore), True, (5, 205, 205)), (2, 30))
+
+    if tankas.boom():
+        busena = "Laukia"
+        hit = pygame.mixer.Sound("explosion2.wav")
+        hit.play()
+        tankas.taikinys()
+        tankas.score += 1
+    screen.blit(enemy, (tankas.enemyx, tankas.enemyy))
+
+    tankas.ugnisx += tankas.ugnisstepx
+    tankas.ugnisy += tankas.ugnisstepy
+
+    if tankas.ugnisx < 0 or tankas.ugnisy < 0 or tankas.ugnisx > gamesizex or tankas.ugnisy > gamesizey:
+        tankas.ugnis()
+    dist = math.sqrt(math.pow(tankas.x - tankas.ugnisx + 24, 2) + math.pow(tankas.y - tankas.ugnisy + 24, 2))
+    if dist < 32 or tankas.enemyx>1000:
+        tankas.enemyx = 2000
+        tankas.enemyy = 2000
+        tankas.ugnisx = 2000
+        tankas.ugnisy = 2000
+        tankas.ugnisstepx = 0
+        tankas.ugnisstepy = 0
+        if tankas.score > highscore:
+            with open("rezultatai.pkl", "wb") as file:
+                pickle.dump(tankas.score, file)
+                highscore = tankas.score
+        screen.blit(tekstas2.render("GAME OVER", True, (165, 205, 25)), (220, 240))
 
     screen.blit(tankas.pozicija, (tankas.x, tankas.y))
+    screen.blit(tekstas.render("Your score: " + str(tankas.score), True, (145, 135, 225)), (2, 2))
+    screen.blit(tekstas.render("Highscore: " + str(highscore), True, (5, 205, 205)), (2, 30))
+    screen.blit(fire, (tankas.ugnisx, tankas.ugnisy))
+
     pygame.display.update()
 
-# while True:
-#     choice1 = input("z - naujas zaidimas\nt - rezultatai\nv - isvalyti rezultatus\ni - iseiti\n")
-#     match choice1:
-#         case "t":
-#             try:
-#                 with open("rezultatai.pkl", "rb") as file:
-#                     highscores = pickle.load(file)
-#                     for x in highscores:
-#                         print(x)
-#             except:
-#                 print("Sarasas tuscias")
-#
-#         case "v":
-#             with open("rezultatai.pkl", "wb") as file:
-#                 a = []
-#                 pickle.dump(a, file)
-#         case "i":
-#             break
-#         case _:
-#             print("Neteisingai pasirinktas veiksmas")
--- tankas.py
+++ tankas.py
@@ -1,11 +1,11 @@
 import random
 import pygame
-import pickle
+import math
 
 
 class Tankas():
-    def __init__(self, gamesizex, gamesizey, pozicija, kulka, taskai=10000000000000, score=0, stepx=0, stepy=0,
-                 stepkulkax=0, stepkulkay =0):
+    def __init__(self, gamesizex, gamesizey, pozicija, kulka, score=0, stepx=0, stepy=0,
+                 stepkulkax=0, stepkulkay=0):
         self.gamesizex = gamesizex
         self.gamesizey = gamesizey
         self.ogpozicija = pozicija
@@ -16,29 +16,13 @@ class Tankas():
         self.stepkulkay = stepkulkay
         self.x = gamesizex / 2 - 32
         self.y = gamesizey - 64
-        self.taskai = taskai
         self.score = score
         self.pozicija = pozicija
-        self.kryptis = ""
+        self.kryptis = "North"
         self.kulka = kulka
         self.kulkax = self.x
         self.kulkay = self.y
         self.taikinys()
-        try:
-            with open("rezultatai.pkl", "rb") as file:
-                self.highscores = pickle.load(file)
-        except:
-            self.highscores = []
-        self.info()
-
-    def taskusekimas(self):
-        if self.taskai <= 0:
-            name = input(f"Zaidimas baigtas, jusu rezultatas: {self.score}, iveskite savo varda: ")
-            with open("rezultatai.pkl", "wb") as file:
-                add = f"{name}: {self.score}"
-                self.highscores.append(add)
-                pickle.dump(self.highscores, file)
-            exit()
 
     def koordinates(self):
         self.x += self.stepx
@@ -59,33 +43,21 @@ class Tankas():
         self.pozicija = self.ogpozicija
         self.kulka = self.ogkulka
         self.kryptis = "North"
-        self.taskai -= 10
-        self.taskusekimas()
-        self.info()
 
     def kairen(self):
         self.pozicija = pygame.transform.rotate(self.ogpozicija, 90)
         self.kulka = pygame.transform.rotate(self.ogkulka, 90)
         self.kryptis = "West"
-        self.taskai -= 10
-        self.taskusekimas()
-        self.info()
 
     def atgal(self):
         self.pozicija = pygame.transform.rotate(self.ogpozicija, 180)
         self.kulka = pygame.transform.rotate(self.ogkulka, 180)
         self.kryptis = "South"
-        self.taskai -= 10
-        self.taskusekimas()
-        self.info()
 
     def desinen(self):
         self.pozicija = pygame.transform.rotate(self.ogpozicija, -90)
         self.kulka = pygame.transform.rotate(self.ogkulka, -90)
         self.kryptis = "East"
-        self.taskai -= 10
-        self.taskusekimas()
-        self.info()
 
     def suvis(self):
         match self.kryptis:
@@ -110,38 +82,25 @@ class Tankas():
                 self.stepkulkax = 0.5
                 self.stepkulkay = 0
 
-
-        print(self.ar_pataike())
-        if self.ar_pataike() == "Pataikyta!":
-            self.taskai += 50
-            self.score += 1
-            self.taikinys()
-            print(f"Sukurtas naujas taikinys {self.taik}")
-        self.info()
-
-    def info(self):
-        print(f"Turimi taskai: {self.taskai}, numusti taikiniai: {self.score}")
-        print(f"Taikinio koordinates: {self.taik}")
-
     def taikinys(self):
-        self.taik = (random.randint(-10, 10), random.randint(-10, 10))
+        self.enemyx = random.randint(0, 736)
+        self.enemyy = random.randint(0, 536)
+        self.ugnis()
 
-    def ar_pataike(self):
-        if self.x == self.taik[0] and self.y == self.taik[1]:
-            return ("Uzvaziuota ant taikinio!!")
-        elif self.x == self.taik[0]:
-            if self.y < self.taik[1] and self.kryptis == "Siaure":
-                return ("Pataikyta!")
-            elif self.y > self.taik[1] and self.kryptis == "Pietus":
-                return ("Pataikyta!")
-            else:
-                return ("Nepataikyta")
-        elif self.y == self.taik[1]:
-            if self.x < self.taik[0] and self.kryptis == "Rytai":
-                return ("Pataikyta!")
-            elif self.x > self.taik[0] and self.kryptis == "Vakarai":
-                return ("Pataikyta!")
-            else:
-                return ("Nepataikyta")
+    def ugnis(self):
+        self.ugnisx = self.enemyx + 28
+        self.ugnisy = self.enemyy + 28
+        # Calculate the vector from the enemy to the object
+        vector_x = self.x - self.enemyx
+        vector_y = self.y - self.enemyy
+        # Calculate the angle between the enemy and the object
+        angle = math.atan2(vector_y, vector_x)
+        self.ugnisstepx = math.cos(angle) * 0.25
+        self.ugnisstepy = math.sin(angle) * 0.25
+
+    def boom(self):
+        dist = math.sqrt(math.pow(self.enemyx - self.kulkax + 24, 2) + math.pow(self.enemyy - self.kulkay + 25, 2))
+        if dist < 32:
+            return True
         else:
-            return ("Nepataikyta")
+            return False
