011cc49f52d69da168c55109d91474bdb4023449
Edvinas Jarmalavicius <edvinas400@gmail.com>
2023-01-06 10:20:54 +0200

    Now it is done (for sure) :)

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ tekstas = pygame.font.Font("sketch.otf", 30)
 tekstas2 = pygame.font.Font("sketch.otf", 100)
 
 busena = "Laukia"
+startas = True
 
 tankas = Tankas(gamesizex, gamesizey, a, kulka)
 
@@ -34,6 +35,8 @@ while running:
     screen.fill((50, 50, 70))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                startas = False
             if event.key == pygame.K_x:
                 endgame = False
                 tankas.taikinys()
@@ -62,46 +65,49 @@ while running:
                 lastkey = None
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
+    if startas:
+        screen.blit(tekstas2.render("WELCOME!", True, (165, 205, 25)), (240, 200))
+        screen.blit(tekstas.render("Press 'P' to play", True, (165, 205, 25)), (300, 300))
+    else:
+        tankas.stepx = -0.1 if lastkey == pygame.K_a else 0.1 if lastkey == pygame.K_d else 0
+        tankas.stepy = -0.1 if lastkey == pygame.K_w else 0.1 if lastkey == pygame.K_s else 0
 
-    tankas.stepx = -0.1 if lastkey == pygame.K_a else 0.1 if lastkey == pygame.K_d else 0
-    tankas.stepy = -0.1 if lastkey == pygame.K_w else 0.1 if lastkey == pygame.K_s else 0
+        if busena == "Issauta":
+            screen.blit(kulka, (tankas.kulkax, tankas.kulkay))
+            tankas.kulkax += tankas.stepkulkax
+            tankas.kulkay += tankas.stepkulkay
+            if tankas.kulkax < 0 or tankas.kulkay < 0 or tankas.kulkax > gamesizex or tankas.kulkay > gamesizey:
+                busena = "Laukia"
 
-    if busena == "Issauta":
-        screen.blit(kulka, (tankas.kulkax, tankas.kulkay))
-        tankas.kulkax += tankas.stepkulkax
-        tankas.kulkay += tankas.stepkulkay
-        if tankas.kulkax < 0 or tankas.kulkay < 0 or tankas.kulkax > gamesizex or tankas.kulkay > gamesizey:
+        tankas.koordinates()
+
+        if tankas.boom():
             busena = "Laukia"
+            hit = pygame.mixer.Sound("explosion2.wav")
+            hit.play()
+            tankas.taikinys()
+            tankas.score += 1
 
-    tankas.koordinates()
+        tankas.ugnisx += tankas.ugnisstepx
+        tankas.ugnisy += tankas.ugnisstepy
 
-    if tankas.boom():
-        busena = "Laukia"
-        hit = pygame.mixer.Sound("explosion2.wav")
-        hit.play()
-        tankas.taikinys()
-        tankas.score += 1
+        if tankas.ugnisx < 0 or tankas.ugnisy < 0 or tankas.ugnisx > gamesizex or tankas.ugnisy > gamesizey:
+            tankas.ugnis()
+        dist = math.sqrt(math.pow(tankas.x - tankas.ugnisx + 25, 2) + math.pow(tankas.y - tankas.ugnisy + 25, 2))
+        if dist < 34:
+            endgame = True
+            if tankas.score > highscore and highscore != tankas.score:
+                with open("rezultatai.pkl", "wb") as file:
+                    pickle.dump(tankas.score, file)
+                    highscore = tankas.score
+        if endgame:
+            screen.blit(tekstas2.render("GAME OVER", True, (165, 205, 25)), (220, 240))
+            screen.blit(tekstas.render("Press 'X' to play again", True, (165, 205, 25)), (280, 340))
 
-    tankas.ugnisx += tankas.ugnisstepx
-    tankas.ugnisy += tankas.ugnisstepy
-
-    if tankas.ugnisx < 0 or tankas.ugnisy < 0 or tankas.ugnisx > gamesizex or tankas.ugnisy > gamesizey:
-        tankas.ugnis()
-    dist = math.sqrt(math.pow(tankas.x - tankas.ugnisx + 25, 2) + math.pow(tankas.y - tankas.ugnisy + 25, 2))
-    if dist < 34:
-        endgame = True
-        if tankas.score > highscore and highscore != tankas.score:
-            with open("rezultatai.pkl", "wb") as file:
-                pickle.dump(tankas.score, file)
-                highscore = tankas.score
-    if endgame:
-        screen.blit(tekstas2.render("GAME OVER", True, (165, 205, 25)), (220, 240))
-        screen.blit(tekstas.render("Press x to play again", True, (165, 205, 25)), (280, 340))
-
-    screen.blit(tankas.pozicija, (tankas.x, tankas.y))
-    screen.blit(tekstas.render("Your score: " + str(tankas.score), True, (145, 135, 225)), (2, 2))
-    screen.blit(tekstas.render("Highscore: " + str(highscore), True, (5, 205, 205)), (2, 30))
-    if endgame == False:
-        screen.blit(enemy, (tankas.enemyx, tankas.enemyy))
-        screen.blit(fire, (tankas.ugnisx, tankas.ugnisy))
+        screen.blit(tankas.pozicija, (tankas.x, tankas.y))
+        screen.blit(tekstas.render("Your score: " + str(tankas.score), True, (145, 135, 225)), (2, 2))
+        screen.blit(tekstas.render("Highscore: " + str(highscore), True, (5, 205, 205)), (2, 30))
+        if endgame == False:
+            screen.blit(enemy, (tankas.enemyx, tankas.enemyy))
+            screen.blit(fire, (tankas.ugnisx, tankas.ugnisy))
     pygame.display.update()
